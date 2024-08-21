bfc2e24aa36cda6fe05f0caa5f454adb54678e9a
Harun Uyguç <harun.uyguc@hotmail.com>
2024-08-21 21:20:28 +0300

    Updated

--- Depo/kutuphane-islemleri.py
+++ Depo/kutuphane-islemleri.py
@@ -1,6 +1,8 @@
 print("pip install seaborn")
 print("pip install pandas_datareader")
 print("pip install scikit-learn")
+print("pip install missingno")
+print("pip install ycimpute")
 
 print("pip uninstall seaborn")
 print("pip list")
